620f8f04f272bb592f24840df84f8631ae59f2f2
Lucasw54 <100497443+Lucasw54@users.noreply.github.com>
2022-12-20 19:35:58 -0500

    Add randomness to the ball dir

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 import pygame
+import random
 from sklearn.model_selection import train_test_split
 
 # Initialize Pygame
@@ -18,60 +19,63 @@ Y = np.zeros((num_games, 1))
 
 # Loop through the number of games to simulate
 for i in range(num_games):
-  # Initialize the game state
-  ball_x = 0.5
-  ball_y = 0.5
-  ball_vx = 0.03
-  ball_vy = 0.01
-  paddle_y = 0.5
-  paddle_v = 0.04
-  
-  # Create the Pygame window
-  screen = pygame.display.set_mode(window_size)
-  
-  # Loop through the time steps in the game
-  for t in range(50):
-    # Update the ball position
-    ball_x += ball_vx
-    ball_y += ball_vy
-    
-    # Check for collision with the walls
-    if ball_y < 0 or ball_y > 1:
-      ball_vy *= -1
-    
-    # Update the paddle position
-    paddle_y += paddle_v
-    
-    # Check for collision with the walls
-    if paddle_y < 0:
-      paddle_y = 0
-    elif paddle_y > 1:
-      paddle_y = 1
-      
-    # Check for collision with the paddle
-    if ball_x < 0 and ball_y > paddle_y and ball_y < paddle_y + 0.2:
-      ball_vx *= -1
-    
-    # Store the game state as a feature
-    X[i, 0] = ball_x
-    X[i, 1] = ball_y
-    X[i, 2] = paddle_y
-    
-    # Store the action as a label
-    if paddle_v > 0:
-      Y[i, 0] = 1
-    else:
-      Y[i, 0] = 0
-    
-    # Draw the ball and paddle on the screen
-    ball_x_px = int(ball_x * window_size[0])
-    ball_y_px = int(ball_y * window_size[1])
-    paddle_y_px = int(paddle_y * window_size[1])
-    pygame.draw.circle(screen, (255, 255, 255), (ball_x_px, ball_y_px), 5)
-    pygame.draw.rect(screen, (255, 255, 255), (0, paddle_y_px, 10, 20))
-    
-    # Update the screen
-    pygame.display.flip()
+    # Initialize the game state
+    ball_x = 0.5
+    ball_y = 0.5
+    ball_vx = random.uniform(-0.05, 0.05)  # Generate a random velocity in the x direction
+    ball_vy = random.uniform(-0.05, 0.05)  # Generate a random velocity in the y direction
+    paddle_y = 0.5
+    paddle_v = 0.04
+
+    # Create the Pygame window
+    screen = pygame.display.set_mode(window_size)
+
+    # Loop through the time steps in the game
+    for t in range(50):
+        # Update the ball position
+        ball_x += ball_vx
+        ball_y += ball_vy
+
+        # Check for collision with the walls
+        if ball_y < 0 or ball_y > 1:
+            ball_vy *= -1
+
+        # Update the paddle position
+        paddle_y += paddle_v
+
+        # Check for collision with the walls
+        if paddle_y < 0:
+            paddle_y = 0
+        elif paddle_y > 1:
+            paddle_y = 1
+
+        # Check for collision with the paddle
+        if ball_x < 0 and ball_y > paddle_y and ball_y < paddle_y + 0.2:
+            ball_vx *= -1
+
+        # Store the game state as a feature
+        X[i, 0] = ball_x
+        X[i, 1] = ball_y
+        X[i, 2] = paddle_y
+
+        # Store the action as a label
+        if paddle_v > 0:
+            Y[i, 0] = 1
+        else:
+            Y[i, 0] = 0
+
+        # Clear the screen
+        screen.fill((0, 0, 0))
+
+        # Draw the ball and paddle on the screen
+        ball_x_px = int(ball_x * window_size[0])
+        ball_y_px = int(ball_y * window_size[1])
+        paddle_y_px = int(paddle_y * window_size[1])
+        pygame.draw.circle(screen, (255, 255, 255), (ball_x_px, ball_y_px), 5)
+        pygame.draw.rect(screen, (255, 255, 255), (0, paddle_y_px, 10, 20))
+
+        # Update the screen
+        pygame.display.flip()
 
 # Split the data into training and testing sets
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
